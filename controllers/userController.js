ab8339222162b022e02564a7d6b7a073aaa3f4e2
Pranav <pranavmurali024@gmail.com>
2024-08-07 00:09:28 +0530

    added user model

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,23 @@
-//@desc Re
-//@route GET /api/contacts
+const asyncHandler = require("express-async-handler");
+//@desc Register a user
+//@route POST /api/users/register
 //@access public
-const getContacts = asyncHandler (async (req, res) => {
-    const contacts = await Contact.find();
-    res.status(200).json(contacts);
+const registerUser = asyncHandler(async (req, res) => {
+    res.json({ message: "Register the user" });
 });
+
+//@desc Login a user
+//@route POST /api/users/login
+//@access public
+const loginUser = asyncHandler(async (req, res) => {
+    res.json({ message: "Login user" });
+});
+
+//@descCurrent user info
+//@route POST /api/users/current
+//@access private
+const currentUser = asyncHandler(async (req, res) => {
+    res.json({ message: "Current user information" });
+});
+
+module.exports = { registerUser, loginUser, currentUser };
