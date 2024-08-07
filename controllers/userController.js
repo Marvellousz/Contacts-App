83e7d05edb9f058b46b742729e5cca18a78d19ba
Pranav <pranavmurali024@gmail.com>
2024-08-07 12:18:42 +0530

    added JWT

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 //@desc Register a user
 //@route POST /api/users/register
@@ -39,7 +40,28 @@ const registerUser = asyncHandler(async (req, res) => {
 //@route POST /api/users/login
 //@access public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({ message: "Login user" });
+    const { email, password } = req.body;
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("Please fill in all fields");
+    }
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign({
+            user: {
+            username: user.username,
+            email: user.email,
+            id: user.id,
+        },
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "1m" }
+    );
+    res.status(200).json({ accessToken });
+    }else {
+        res.status(401)
+        throw new Error("Invalid email or password");
+    }
 });
 
 //@descCurrent user info
